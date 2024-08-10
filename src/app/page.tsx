0ca27272ef9c7451661d100e56fbc3ed2b44c2fc
Sudeep Paul <sudeeprnp@gmail.com>
2024-08-10 21:43:08 +0530

    removed deplyment errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
+// import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Blog from "@/components/Blog";
 import Brands from "@/components/Brands";
