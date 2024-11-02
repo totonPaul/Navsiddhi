2fcba86e1cb488cc2019eea3ad9fce08645c7398
Sudeep Paul <sudeeprnp@gmail.com>
2024-11-02 16:11:23 +0530

    social media links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -31,6 +31,50 @@ const Footer = () => {
                 </p>
                 <div className="flex items-center">
                   <a
+                    href="https://www.instagram.com/navsiddhi_org" // Replace with your Instagram link
+                    aria-label="instagram-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mr-6 duration-300 hover:opacity-80"
+                  >
+                    <img
+                      src="/images/instagram.jpg"
+                      alt="Instagram"
+                      width={24}
+                      height={24}
+                    />
+                  </a>
+                  <a
+                    href="https://www.youtube.com/@ClickVidya.navsiddhi" // Replace with your YouTube link
+                    aria-label="youtube-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mr-6 duration-300 hover:opacity-80"
+                  >
+                    <img
+                      src="/images/youtube.png"
+                      alt="YouTube"
+                      width={24}
+                      height={24}
+                    />
+                  </a>
+                  <a
+                    href="mailto:navsiddhi.org@gmail.com" // Replace with your Gmail address
+                    aria-label="gmail-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="duration-300 hover:opacity-80"
+                  >
+                    <img
+                      src="/images/gmail.jpg"
+                      alt="Gmail"
+                      width={24}
+                      height={24}
+                    />
+                  </a>
+                </div>
+                {/* <div className="flex items-center">
+                  <a
                     href="/"
                     aria-label="social-link"
                     target="_blank"
@@ -104,7 +148,7 @@ const Footer = () => {
                       <path d="M15.2196 0H1.99991C1.37516 0 0.875366 0.497491 0.875366 1.11936V14.3029C0.875366 14.8999 1.37516 15.4222 1.99991 15.4222H15.1696C15.7943 15.4222 16.2941 14.9247 16.2941 14.3029V1.09448C16.3441 0.497491 15.8443 0 15.2196 0ZM5.44852 13.1089H3.17444V5.7709H5.44852V13.1089ZM4.29899 4.75104C3.54929 4.75104 2.97452 4.15405 2.97452 3.43269C2.97452 2.71133 3.57428 2.11434 4.29899 2.11434C5.02369 2.11434 5.62345 2.71133 5.62345 3.43269C5.62345 4.15405 5.07367 4.75104 4.29899 4.75104ZM14.07 13.1089H11.796V9.55183C11.796 8.7061 11.771 7.58674 10.5964 7.58674C9.39693 7.58674 9.222 8.53198 9.222 9.47721V13.1089H6.94792V5.7709H9.17202V6.79076H9.19701C9.52188 6.19377 10.2466 5.59678 11.3711 5.59678C13.6952 5.59678 14.12 7.08925 14.12 9.12897V13.1089H14.07Z" />
                     </svg>
                   </a>
-                </div>
+                </div> */}
               </div>
             </div>
 
@@ -142,39 +186,6 @@ const Footer = () => {
               </div>
             </div>
 
-            <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
-              <div className="mb-12 lg:mb-16">
-                <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
-                  Terms
-                </h2>
-                <ul>
-                  <li>
-                    <Link
-                      href="/"
-                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                    >
-                      TOS
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/"
-                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                    >
-                      Privacy Policy
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/"
-                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
-                    >
-                      Refund Policy
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
 
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-3/12">
               <div className="mb-12 lg:mb-16">
@@ -187,7 +198,7 @@ const Footer = () => {
                       href="/contact"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      Open Support Ticket
+                      9431653636
                     </Link>
                   </li>
                   <li>
